4202e9228ebac4b858db1b5322e83cde74ba4f21
Pieter <pieter@sensingit.nl>
2020-10-09 13:32:19 +0100

    add

---
+++ workshopzombiecopy/Assets/Scripts/StalkScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalkScript : MonoBehaviour
+{
+	public Transform Player;
+	public double MoveSpeed = 0.5;
+	public int MinDist = 5;
+	 
+	UnityEngine.AI.NavMeshAgent myNavMeshAgent;
+  
+	void Start () 
+	{
+		myNavMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+ 
+	void Update () 
+	{
+		transform.LookAt(Player);
+		
+		if(Vector3.Distance(transform.position, Player.position) >= MinDist){
+			myNavMeshAgent.destination = Player.position;
+		}
+	}
+}

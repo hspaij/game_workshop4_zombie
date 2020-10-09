cf4295a611a4b66d8ae51bb0fb9546a01af1a363
Pieter <pieter@sensingit.nl>
2020-10-09 10:10:15 +0100

    Add object scripts

---
+++ workshopzombiecopy/Assets/Scripts/InteractWithObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithObject : MonoBehaviour
+{
+	public float pickupRadius = 2.0f;
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+			print("Trying to pick up");
+			
+			Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, pickupRadius);
+			foreach (var hitCollider in hitColliders)
+			{
+				if(hitCollider.gameObject.tag != "Player" && hitCollider.gameObject.tag != "Untagged") {
+					print(hitCollider.gameObject.tag);
+				}
+			}
+		}
+    }
+}
--- workshopzombiecopy/Assets/Scripts/OpenDoor.cs
+++ workshopzombiecopy/Assets/Scripts/OpenDoor.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetItem : MonoBehaviour
+public class OpenDoor : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
